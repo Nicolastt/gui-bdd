7e264cde11305f1a57ecff99b14c05ce32c52102
Nicholas <gianfrancopr08@gmail.com>
2024-07-19 18:39:43 -0500

    Se creó un pie de página.

---
+++ frontend/src/Tail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tail.css'; // Asegúrate de crear este archivo para los estilos
+
+const Tail = () => (
+    <div className="tail">
+        <p>Proyecto realizado por:</p>
+        <ul>
+            <li>Anthony Morales</li>
+            <li>Ariel Amaguaña</li>
+            <li>Gianfranco Pupiales</li>
+        </ul>
+    </div>
+);
+
+export default Tail;
