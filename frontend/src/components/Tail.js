caa5fc596db5ed9dc00477812181ee3364e7505a
Nicholas <gianfrancopr08@gmail.com>
2024-07-19 19:07:35 -0500

    Optimizar Tail.

--- frontend/src/components/Tail.js
+++ frontend/src/components/Tail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Tail.css'; // Asegúrate de crear este archivo para los estilos
+import '../css/Tail.css'; // Asegúrate de crear este archivo para los estilos
 
 const Tail = () => (
     <div className="tail">
