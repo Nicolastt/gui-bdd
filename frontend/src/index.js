e06fa412e8b551b09e5ece0de0fc2edab531839a
Nicholas <gianfrancopr08@gmail.com>
2024-08-01 22:29:00 -0500

    Agregar al index el componente Auditoria

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,10 +9,11 @@ import Ordenes from "./components/Ordenes";
 import Proveedores from "./components/Proveedores";
 import Productos from "./components/Productos";
 import DetalleOrdenes from "./components/DetalleOrdenes";
+import Auditoria from "./components/Auditoria";
 import Tail from "./components/Tail";
 
 ReactDOM.render(
-        <>
+    <>
         <h1 className="centered-heading">CRUD PROYECTO BASE DE DATOS DISTRIBUIDAS</h1>
         <Categorias/>
         <Clientes/>
@@ -21,9 +22,10 @@ ReactDOM.render(
         <Proveedores/>
         <Productos/>
         <DetalleOrdenes/>
+        <Auditoria/>
         <Tail/>
-        </>,
-        document.getElementById('root')
+    </>,
+    document.getElementById('root')
 );
 
 serviceWorker.unregister();
