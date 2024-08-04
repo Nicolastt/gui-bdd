e4bee965d7eef26a3406c52c2c5d17efc3311556
Nicholas <gianfrancopr08@gmail.com>
2024-08-04 15:57:20 -0500

    Se aplicó el diseño de auditoria para solo ese componente.

--- frontend/src/components/Auditoria.js
+++ frontend/src/components/Auditoria.js
@@ -21,30 +21,30 @@ const Auditoria = () => {
     }, []);
 
     return (
-        <div className="container-auditoria">
-            <h1>Auditoría</h1>
-            <Table striped>
-                <thead>
-                <tr>
-                    <th>Usuario</th>
-                    <th>Fecha</th>
-                    <th>Tipo de Operación</th>
-                    <th>Nombre de la Tabla</th>
-                    <th>Valor Anterior</th>
-                    <th>Valor Nuevo</th>
-                </tr>
+        <div className="auditoria-container">
+            <h1 className="auditoria-title">Auditoría</h1>
+            <Table className="auditoria-table" striped>
+                <thead className="auditoria-thead">
+                    <tr>
+                        <th className="auditoria-th">Usuario</th>
+                        <th className="auditoria-th">Fecha</th>
+                        <th className="auditoria-th">Tipo de Operación</th>
+                        <th className="auditoria-th">Nombre de la Tabla</th>
+                        <th className="auditoria-th">Valor Anterior</th>
+                        <th className="auditoria-th">Valor Nuevo</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {data.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.user_name}</td>
-                        <td>{item.fecha}</td>
-                        <td>{item.tipo_operacion}</td>
-                        <td>{item.nombre_table}</td>
-                        <td>{item.anterior}</td>
-                        <td>{item.nuevo}</td>
-                    </tr>
-                ))}
+                    {data.map((item, index) => (
+                        <tr key={index} className="auditoria-tr">
+                            <td className="auditoria-td">{item.user_name}</td>
+                            <td className="auditoria-td">{item.fecha}</td>
+                            <td className="auditoria-td">{item.tipo_operacion}</td>
+                            <td className="auditoria-td">{item.nombre_table}</td>
+                            <td className="auditoria-td">{item.anterior}</td>
+                            <td className="auditoria-td">{item.nuevo}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </Table>
         </div>
