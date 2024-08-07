8beee461cd31124f38d48ef9ad2056fb121552a7
Nicholas <gianfrancopr08@gmail.com>
2024-08-07 16:44:18 -0500

    Ya se pone por encima la etiqueta.

--- frontend/src/components/Notificacion.js
+++ frontend/src/components/Notificacion.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Alert, Container, Row, Col } from 'reactstrap';
 
 const Notificacion = ({ mensaje, tipo }) => {
   const alertColor = tipo === 'success' ? 'success' : 'danger';
 
-  return (
-    <Container fluid className="d-flex justify-content-center align-items-center" style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 1050 }}>
+  return ReactDOM.createPortal(
+    <Container fluid className="d-flex justify-content-center align-items-center" style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 9999 }}>
       <Row className="w-100">
         <Col>
           <Alert color={alertColor} className="text-center w-100 m-0">
@@ -13,7 +14,8 @@ const Notificacion = ({ mensaje, tipo }) => {
           </Alert>
         </Col>
       </Row>
-    </Container>
+    </Container>,
+    document.body // Se renderiza en el nivel superior del DOM
   );
 };
 
