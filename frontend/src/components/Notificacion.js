514ab3f483fba32c9f43bff5e549130b3eb570c2
Nicholas <gianfrancopr08@gmail.com>
2024-08-07 16:48:44 -0500

    El mensaje ya se muestra por encima

--- frontend/src/components/Notificacion.js
+++ frontend/src/components/Notificacion.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Alert, Container, Row, Col } from 'reactstrap';
+import {Alert, Col, Container, Row} from 'reactstrap';
 
-const Notificacion = ({ mensaje, tipo }) => {
-  const alertColor = tipo === 'success' ? 'success' : 'danger';
+const Notificacion = ({mensaje, tipo}) => {
+    const alertColor = tipo === 'success' ? 'success' : 'danger';
 
-  return ReactDOM.createPortal(
-    <Container fluid className="d-flex justify-content-center align-items-center" style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 9999 }}>
-      <Row className="w-100">
-        <Col>
-          <Alert color={alertColor} className="text-center w-100 m-0">
-            {mensaje}
-          </Alert>
-        </Col>
-      </Row>
-    </Container>,
-    document.body // Se renderiza en el nivel superior del DOM
-  );
+    return ReactDOM.createPortal(
+        <Container fluid className="d-flex justify-content-center align-items-center"
+                   style={{position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 9999}}>
+            <Row className="w-100">
+                <Col>
+                    <Alert color={alertColor} className="text-center w-100 m-0">
+                        {mensaje}
+                    </Alert>
+                </Col>
+            </Row>
+        </Container>,
+        document.body
+    );
 };
 
 export default Notificacion;
